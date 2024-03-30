da19d6860910590df4810713f30b9eb9aabb4b13
kuldeep <kuldeepsaini5514@gmail.com>
2024-03-30 22:35:11 +0530

    api fix

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 200000); 
+    }, 2000); 
 
     return () => clearTimeout(timer); 
   }, []);
