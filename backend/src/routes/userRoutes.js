7b6ec2d628e3fe07f90f3f89dd5b591dc336613e
kuldeep saini <kuldeepsaini5514@gmail.com>
2024-11-29 01:59:10 +0530

    Refresh User Expired Token

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/UserController.js";
 import { Router } from "express";
@@ -9,22 +10,9 @@ import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post(
-  "/register",
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-    {
-      name: "coverImage",
-      maxCount: 1,
-    },
-  ]),
-  registerUser
-);
-
+router.post("/register",upload.fields([{name: "avatar",maxCount: 1,},{name: "coverImage",maxCount: 1,},]),registerUser);
 router.post("/login", loginUser);
 router.get("/logout", verifyToken, logoutUser);
+router.get("/refresh_token", refreshAccessToken);
 
 export default router;
